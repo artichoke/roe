71a012d9afbc24bead9a006305847e49cea6d2da
artichoke-ci <ci@artichokeruby.org>
2021-01-29 15:15:11 +0000

    deploy: f47f9f37f0a6e08ac3dc7a2fb2d58004a928dd64

--- implementors/core/str/traits/trait.FromStr.js
+++ implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["roe"] = [{"text":"impl FromStr for LowercaseMode","synthetic":false,"types":[]}];
+implementors["roe"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"roe/enum.LowercaseMode.html\" title=\"enum roe::LowercaseMode\">LowercaseMode</a>","synthetic":false,"types":["roe::LowercaseMode"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["roe"] = [{"text":"impl Error for InvalidLowercaseMode","synthetic":false,"types":[]}];
+implementors["roe"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"roe/struct.InvalidLowercaseMode.html\" title=\"struct roe::InvalidLowercaseMode\">InvalidLowercaseMode</a>","synthetic":false,"types":["roe::InvalidLowercaseMode"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
