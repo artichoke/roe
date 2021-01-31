bb9720e8075112724d1312390082a44b66a7f32b
artichoke-ci <ci@artichokeruby.org>
2021-01-31 16:06:37 +0000

    deploy: 831f41fa8ef54f81cd045a0f2a467c77ea88b7d2

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["bstr"] = {"name":"","dirs":[{"name":"byteset","files":["mod.rs","scalar.rs"]},{"name":"search","files":["byte_frequencies.rs","mod.rs","prefilter.rs","twoway.rs"]}],"files":["ascii.rs","bstr.rs","cow.rs","ext_slice.rs","impls.rs","lib.rs","utf8.rs"]};
 sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
-sourcesIndex["roe"] = {"name":"","dirs":[{"name":"lowercase","files":["ascii.rs","full.rs"]}],"files":["lib.rs","lowercase.rs","uppercase.rs"]};
+sourcesIndex["roe"] = {"name":"","dirs":[{"name":"ascii","files":["lowercase.rs","titlecase.rs","uppercase.rs"]},{"name":"lowercase","files":["ascii.rs","full.rs"]}],"files":["ascii.rs","lib.rs","lowercase.rs","uppercase.rs"]};
 createSourceSidebar();
