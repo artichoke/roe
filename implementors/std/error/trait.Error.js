26f637ccdcc8198132623195709f7c03904597a3
artichoke-ci <ci@artichokeruby.org>
2021-02-15 02:56:11 +0000

    deploy: e25ce89837453d5c8504d89a00aba49e1f8bf00a

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["roe"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"roe/struct.InvalidLowercaseMode.html\" title=\"struct roe::InvalidLowercaseMode\">InvalidLowercaseMode</a>","synthetic":false,"types":["roe::InvalidLowercaseMode"]}];
+implementors["roe"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"roe/struct.InvalidCaseMappingMode.html\" title=\"struct roe::InvalidCaseMappingMode\">InvalidCaseMappingMode</a>","synthetic":false,"types":["roe::InvalidCaseMappingMode"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
